c99d6a70737bc8d3371145b4f42e84713516f8a1
Mark Page <mark.page@whitelamp.com>
2018-02-19 19:26:21 +0000

    Export class to extend SwefExport

---
+++ app/class/Swef/Bespoke/Export.class.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Swef\Bespoke;
+
+class Export extends \Swef\Base\SwefExport {
+
+    public function __construct ( ) {
+        // Always construct the base class - PHP does not do this implicitly
+        parent::__construct ();
+    }
+
+    public function __destruct ( ) {
+        // Always destruct the base class - PHP does not do this implicitly
+        parent::__destruct ();
+    }
+
+}
+
+?>
